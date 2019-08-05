bce5ebbefd85f74f92facd596831953fd1232f28
Jimmy Caldrone <luaujimmy@gmail.com>
2019-08-04 23:31:42 -0500

    vv

---
+++ Stickers/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Stickers
+{
+    partial class Sticker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sticker));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PinToolStripItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 425);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripFile,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripFile
+            // 
+            this.ToolStripFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.PinToolStripItem,
+            this.saveToolStripMenuItem});
+            this.ToolStripFile.Name = "ToolStripFile";
+            this.ToolStripFile.Size = new System.Drawing.Size(37, 20);
+            this.ToolStripFile.Text = "File";
+            this.ToolStripFile.Click += new System.EventHandler(this.NewToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItem_Click_1);
+            // 
+            // PinToolStripItem
+            // 
+            this.PinToolStripItem.Name = "PinToolStripItem";
+            this.PinToolStripItem.Size = new System.Drawing.Size(180, 22);
+            this.PinToolStripItem.Text = "Pin";
+            this.PinToolStripItem.Click += new System.EventHandler(this.PinToolStripItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.toolStripMenuItem1.CheckOnClick = true;
+            this.toolStripMenuItem1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(36, 20);
+            this.toolStripMenuItem1.Text = "Pin";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem1_Click);
+            // 
+            // Sticker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Sticker";
+            this.Text = "Sticker";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripFile;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem PinToolStripItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ Stickers/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Stickers
+{
+    public partial class Sticker : Form
+    {
+        static string FilePath = "";
+        static string FileName = "";
+        public Sticker()
+        {
+            InitializeComponent();
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PinnedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Sticker.ActiveForm.TopMost = true;
+        }
+
+        private void UnpinnedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Sticker.ActiveForm.TopMost = false;
+        }
+
+        private void ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            HandlePin();
+        }
+
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Task.Run(() => Application.Run(new Sticker()));
+        }
+
+        private void PinToolStripItem_Click(object sender, EventArgs e)
+        {
+            HandlePin();
+        }
+
+        private void HandlePin()
+        {
+            if (Sticker.ActiveForm.TopMost)
+            {
+                toolStripMenuItem1.Checked = false;
+                Sticker.ActiveForm.TopMost = false;
+                PinToolStripItem.Checked = false;
+            }
+            else
+            {
+                toolStripMenuItem1.Checked = true;
+                Sticker.ActiveForm.TopMost = true;
+                PinToolStripItem.Checked = true;
+            }
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (FilePath == "")
+            {
+                SaveAs();
+            }
+            Save();
+        }
+        private void SaveAs()
+        {
+            var SaveDialog = new SaveFileDialog();
+            SaveDialog.ShowDialog();
+            FileName = SaveDialog.FileName;
+            FilePath = Path.GetFullPath(FileName);
+            var lines = richTextBox1.Text.Split('\n');
+            File.WriteAllLines(FilePath, lines);
+        }
+
+        private void Save()
+        {
+            var lines = richTextBox1.Text.Split('\n');
+            File.WriteAllLines(FilePath, lines);
+        }
+
+    }
+}
